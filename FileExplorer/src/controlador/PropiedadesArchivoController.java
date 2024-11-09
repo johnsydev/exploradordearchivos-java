fa3be89f0f2ce1f35cf8fd19dfabc04e9e2660af
Johnsy López <90042591+johnsydev@users.noreply.github.com>
2024-11-08 22:28:23 -0600

    Controlador de propiedades de un archivo
    Fecha: 08/11/2024
    Horas: 9 PM - 10:30 PM
    Estudiante: Johnsy (solo)

---
+++ FileExplorer/src/controlador/PropiedadesArchivoController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controlador;
+
+import logicadenegocios.Archivo;
+import vista.PropiedadesArchivoForm;
+
+/**
+ *
+ * @author johns
+ */
+public class PropiedadesArchivoController {
+  private Archivo modelo;
+  private PropiedadesArchivoForm vista;
+  
+  public PropiedadesArchivoController(Archivo pArchivo) {
+    modelo = pArchivo;
+    vista = new PropiedadesArchivoForm();
+  }
+  
+  public void actualizar() {
+    vista.setNombre("modelo.getNombre()");
+    
+  }
+  
+}
