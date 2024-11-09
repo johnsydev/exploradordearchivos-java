8e523d3dffe22a83a552337ffb95930d117fbd37
Johnsy López <90042591+johnsydev@users.noreply.github.com>
2024-11-08 22:26:46 -0600

    Vista de propiedades de un archivo actualizada

--- FileExplorer/src/vista/PropiedadesArchivoForm.java
+++ FileExplorer/src/vista/PropiedadesArchivoForm.java
@@ -14,6 +14,10 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class PropiedadesArchivoForm extends JFrame {
+  
+  JTextField nombre;
+  JLabel extension;
+  JLabel ubicacion;
 
   public PropiedadesArchivoForm() {
     // Configuración de la ventana principal
@@ -29,18 +33,25 @@ public class PropiedadesArchivoForm extends JFrame {
     
 
     JPanel panel = new JPanel();
-    panel.setLayout(new GridLayout(2, 2));
+    panel.setLayout(new GridLayout(3, 2));
     
     JLabel nombreLabel = new JLabel("Nombre:");
-    JTextField nombre = new JTextField("test.txt");
+    nombre = new JTextField("");
     nombre.setEditable(false);
     nombre.setFocusable(false);
     
     JLabel extensionLabel = new JLabel("Extensión:");
+    extension = new JLabel("");
+    
+    JLabel ubicacionLabel = new JLabel("Ubicación:");
+    ubicacion = new JLabel("");
     
     panel.add(nombreLabel);
     panel.add(nombre);
     panel.add(extensionLabel);
+    panel.add(extension);
+    panel.add(ubicacionLabel);
+    panel.add(ubicacion);
     
     /*
     botonVolver = new JButton("Volver");
@@ -58,6 +69,18 @@ public class PropiedadesArchivoForm extends JFrame {
     
     
     setVisible(true);
+  }
+  
+  public void setNombre(String pNombre) {
+    nombre.setText(pNombre);
+  }
+  
+  public void setExtension(String pExtension) {
+    extension.setText(pExtension);
+  }
+  
+  public void setUbicacion(String pUbicacion) {
+    ubicacion.setText(pUbicacion);
   }
 }
 
