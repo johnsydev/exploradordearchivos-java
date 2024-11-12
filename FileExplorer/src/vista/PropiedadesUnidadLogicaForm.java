915179b46cb0a944ff110b7e11ec0283afd38a65
Julián Andrés Lizano Monge <160781158+Jlm0616@users.noreply.github.com>
2024-11-12 16:33:43 -0600

    Ventana funcional, faltan la logica interna

--- FileExplorer/src/vista/PropiedadesUnidadLogicaForm.java
+++ FileExplorer/src/vista/PropiedadesUnidadLogicaForm.java
@@ -26,18 +26,19 @@ import logicadenegocios.UnidadLogica;
 public class PropiedadesUnidadLogicaForm extends JFrame {
 
   private final JTextField nombre;
+  private final JTextField tipo;
   private final JTextField espacioTotal;
   private final JTextField espacioUsado;
   private final JTextField espacioLibre;
   //private final JProgressBar barraEspacio;
-  private final JTextField tipo;
   private final JTextField sistemaArchivos;
 
-  public final double TAMANO_COLUMNA_1 = 0.3;
-  public final double TAMANO_COLUMNA_2 = 1.5;
+  public final double TAMANO_COLUMNA_1 = 0.2;
+  public final double TAMANO_COLUMNA_2 = 1.2;
 
   public PropiedadesUnidadLogicaForm() {
-    setTitle("Propiedades de Unidad");
+    // Configuración de la ventana principal
+    setTitle("Propiedades");
     setSize(370, 540);
     setLayout(new BorderLayout());
     setResizable(false);
@@ -57,14 +58,15 @@ public class PropiedadesUnidadLogicaForm extends JFrame {
     GridBagConstraints gbc = new GridBagConstraints();
     gbc.fill = GridBagConstraints.HORIZONTAL;
 
-    Border sangria = BorderFactory.createEmptyBorder(0, 10, 0, 0); // Aumenté la sangría a 6 para mayor separación
+    Border sangria = BorderFactory.createEmptyBorder(0, 10, 0, 0);
+    Border sangria2 = BorderFactory.createEmptyBorder(0, 22, 0, 0);
 
     gbc.gridx = 0;
     gbc.gridy = 0;
     gbc.gridwidth = 2;
     panel.add(new JLabel(" "), gbc); // Espacio antes de la fila 0
 
-    // Fila 0: "Nombre"
+// Fila 0: "Nombre"
     gbc.gridx = 0;
     gbc.gridy = 1;
     gbc.weightx = TAMANO_COLUMNA_1;
@@ -81,13 +83,13 @@ public class PropiedadesUnidadLogicaForm extends JFrame {
     gbc.weightx = TAMANO_COLUMNA_2;
     panel.add(nombre, gbc);
 
-    // Espacio
+// Espacio
     gbc.gridx = 0;
     gbc.gridy = 2;
     gbc.gridwidth = 2;
     panel.add(new JLabel(" "), gbc); // Espacio adicional después de la línea debajo de "Nombre"
 
-    // Línea separadora
+// Línea separadora entre "Nombre" y "Tipo de Archivo"
     gbc.gridx = 0;
     gbc.gridy = 3;
     gbc.gridwidth = 2;
@@ -95,15 +97,16 @@ public class PropiedadesUnidadLogicaForm extends JFrame {
     line1.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY));
     panel.add(line1, gbc);
 
-    // Espacio
+// Espacio
     gbc.gridx = 0;
     gbc.gridy = 4;
     gbc.gridwidth = 2;
     panel.add(new JLabel(" "), gbc); // Espacio entre filas
 
-    // Fila 1: "Tipo"
+// Fila 1: "Tipo de Archivo"
     gbc.gridx = 0;
     gbc.gridy = 5;
+    gbc.gridwidth = 1;
     gbc.weightx = TAMANO_COLUMNA_1;
     JLabel lblTipo = new JLabel("Tipo:");
     lblTipo.setBorder(sangria);
@@ -112,61 +115,60 @@ public class PropiedadesUnidadLogicaForm extends JFrame {
     tipo = new JTextField("");
     tipo.setEditable(false);
     tipo.setFocusable(false);
-    tipo.setBackground(Color.WHITE);
-    tipo.setBorder(BorderFactory.createEmptyBorder());
     gbc.gridx = 1;
     gbc.weightx = TAMANO_COLUMNA_2;
     panel.add(tipo, gbc);
 
+// Espacio 
     gbc.gridx = 0;
     gbc.gridy = 6;
     gbc.gridwidth = 2;
-    panel.add(new JLabel(" "), gbc); // Espacio entre filas  
+    panel.add(new JLabel(" "), gbc); // Espacio entre filas
 
-    // Fila 2: "Sistema de Archivos"
+// Fila 2: "Extensión"
     gbc.gridx = 0;
     gbc.gridy = 7;
+    gbc.gridwidth = 1;
     gbc.weightx = TAMANO_COLUMNA_1;
-    JLabel lblSistemaDeArchivos = new JLabel("Sistema de Archivos:");
-    lblSistemaDeArchivos.setBorder(sangria);
-    panel.add(lblSistemaDeArchivos, gbc);
+    JLabel lblExtension = new JLabel("Sistema de archivos:");
+    lblExtension.setBorder(sangria);
+    panel.add(lblExtension, gbc);
 
     sistemaArchivos = new JTextField("");
     sistemaArchivos.setEditable(false);
     sistemaArchivos.setFocusable(false);
-    sistemaArchivos.setBackground(Color.WHITE);
-    sistemaArchivos.setBorder(BorderFactory.createEmptyBorder());
     gbc.gridx = 1;
     gbc.weightx = TAMANO_COLUMNA_2;
     panel.add(sistemaArchivos, gbc);
 
-    // Espacio
+// Espacio 
     gbc.gridx = 0;
     gbc.gridy = 8;
     gbc.gridwidth = 2;
-    panel.add(new JLabel(" "), gbc); // Espacio adicional después de la línea debajo de "Nombre"
+    panel.add(new JLabel(" "), gbc); // Espacio entre filas
 
-    // Línea separadora entre "Nombre" y "Tipo de Archivo"
+// Línea separadora
     gbc.gridx = 0;
     gbc.gridy = 9;
     gbc.gridwidth = 2;
     JLabel line2 = new JLabel();
-    line2.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY));
+    line2.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY)); // Línea gris claro
     panel.add(line2, gbc);
 
-    // Espacio
+// Espacio 
     gbc.gridx = 0;
     gbc.gridy = 10;
     gbc.gridwidth = 2;
     panel.add(new JLabel(" "), gbc); // Espacio entre filas
 
-    // Fila 3: "Espacio Usado"
+// Fila 5: "Fecha de Creación"
     gbc.gridx = 0;
     gbc.gridy = 11;
+    gbc.gridwidth = 1;
     gbc.weightx = TAMANO_COLUMNA_1;
-    JLabel lblEspacioUsado = new JLabel("Espacio Usado:");
-    lblEspacioUsado.setBorder(sangria);
-    panel.add(lblEspacioUsado, gbc);
+    JLabel lblFechaCreacion = new JLabel("Estado Usado:");
+    lblFechaCreacion.setBorder(sangria);
+    panel.add(lblFechaCreacion, gbc);
 
     espacioUsado = new JTextField("");
     espacioUsado.setEditable(false);
@@ -177,19 +179,20 @@ public class PropiedadesUnidadLogicaForm extends JFrame {
     gbc.weightx = TAMANO_COLUMNA_2;
     panel.add(espacioUsado, gbc);
 
-    // Espacio
+// Espacio 
     gbc.gridx = 0;
     gbc.gridy = 12;
     gbc.gridwidth = 2;
     panel.add(new JLabel(" "), gbc); // Espacio entre filas
 
-    // Fila 4: "Espacio Disponible"
+// Fila 6: "Fecha de Modificacion"
     gbc.gridx = 0;
     gbc.gridy = 13;
+    gbc.gridwidth = 1;
     gbc.weightx = TAMANO_COLUMNA_1;
-    JLabel lblEspacioDisponible = new JLabel("Espacio disponible:");
-    lblEspacioDisponible.setBorder(sangria);
-    panel.add(lblEspacioDisponible, gbc);
+    JLabel lblFechaModificacion = new JLabel("Espacio Disponible:");
+    lblFechaModificacion.setBorder(sangria);
+    panel.add(lblFechaModificacion, gbc);
 
     espacioLibre = new JTextField("");
     espacioLibre.setEditable(false);
@@ -200,33 +203,34 @@ public class PropiedadesUnidadLogicaForm extends JFrame {
     gbc.weightx = TAMANO_COLUMNA_2;
     panel.add(espacioLibre, gbc);
 
-    // Espacio
+    // Espacio 
     gbc.gridx = 0;
     gbc.gridy = 14;
     gbc.gridwidth = 2;
-    panel.add(new JLabel(" "), gbc); // Espacio adicional después de la línea debajo de "Nombre"
+    panel.add(new JLabel(" "), gbc); // Espacio entre filas
 
-    // Línea separadora
+// Línea separadora
     gbc.gridx = 0;
     gbc.gridy = 15;
     gbc.gridwidth = 2;
     JLabel line3 = new JLabel();
-    line3.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY));
+    line3.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.LIGHT_GRAY)); // Línea gris claro
     panel.add(line3, gbc);
 
-    // Espacio
+// Espacio 
     gbc.gridx = 0;
     gbc.gridy = 16;
     gbc.gridwidth = 2;
     panel.add(new JLabel(" "), gbc); // Espacio entre filas
 
-    // Fila 5: "Capacidad"
+    // Fila 8: "Atributos"
     gbc.gridx = 0;
     gbc.gridy = 17;
+    gbc.gridwidth = 1;
     gbc.weightx = TAMANO_COLUMNA_1;
-    JLabel lblEspacioTotal = new JLabel("Capacidad:");
-    lblEspacioTotal.setBorder(sangria);
-    panel.add(lblEspacioTotal, gbc);
+    JLabel lbAtributos = new JLabel("Capacidad");
+    lbAtributos.setBorder(sangria2);
+    panel.add(lbAtributos, gbc);
 
     espacioTotal = new JTextField("");
     espacioTotal.setEditable(false);
